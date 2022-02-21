206695c4ccee995707709114b220c9bea37f27a6
lijflzi <99627543+lijflzi@users.noreply.github.com>
2022-02-21 09:23:08 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  
+  <title>My Home Page</title>
+ </head>
+ <body>
+ <h1>Welcome to my Website! I am {Elijah Faeldonea} !</h1>
+ <ul>
+     <li><a href="index.php">HOME</a></li>
+     <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+ </ul>
+ </body>
+</html> 
+ 
