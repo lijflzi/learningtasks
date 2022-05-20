8ce42ae2ce9b1039e1ad29df8364d8985c11d1fa
lijflzi <99627543+lijflzi@users.noreply.github.com>
2022-05-20 12:30:58 +0000

    Add files via upload

---
+++ LIJLT6/mygallery.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+
+<html>
+    
+	<head>
+		<title> Home </title>
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">       
+        <link rel= "stylesheet" href= "gallery.css">
+    </head>
+    
+    
+            <nav>
+                <ul>
+                    <li><a href = "index.php"> Home </a></li>
+                    <li><a href = "profile.php"> Creator's Profile </a></li>
+                    <li><a href = "project.php"> Projects </a></li>
+                    <li><a href = "mygallery.php"> Gallery </a></li>
+                </ul>
+            </nav> 
+            
+            
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="favorite.gif">
+      <img src="favorite.gif" alt="Cinque Terre" width="600" height="400">
+    </a>
+    <div class="desc">favOriTe</div>
+  </div>
+</div>
+
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="high.gif">
+      <img src="high.gif" alt="Forest" width="600" height="400">
+    </a>
+    <div class="desc">Hi-High</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="butterfly.gif">
+      <img src="butterfly.gif" alt="Northern Lights" width="600" height="400">
+    </a>
+    <div class="desc">Butterfly</div>
+  </div>
+</div>
+
+<div class="responsive">
+  <div class="gallery">
+    <a target="_blank" href="star.gif">
+      <img src="star.gif" alt="Mountains" width="600" height="400">
+    </a>
+    <div class="desc">Star</div>
+  </div>
+</div>
+
+<div class="clearfix"></div>
+
+<div style="padding:6px;">
+  <p></p>
+  <p></p>
+</div>
+
+</body>
+</html>
+
+
+    
---
+++ LIJLT6/project.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+    
+	<head>
+		<title> Home </title>
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">       
+        <link rel= "stylesheet" href= "project.css">
+    </head>
+    <nav>
+                <ul>
+                    <li><a href = "index.php"> Home </a></li>
+                    <li><a href = "profile.php"> Creator's Profile </a></li>
+                    <li><a href = "project.php"> Projects </a></li>
+                    <li><a href = "mygallery.php"> Gallery </a></li>
+                </ul>
+            </nav> 
+        </div>
+    </div>
+
+    <body>
+    <h1>Projects</h1>
+       
+            <li><a href = LT2/index.html> Simple Recipes</a></li>
+            <li><a href = LT3/index.html> Learning Task 3 </a></li>
+            <li><a href = LT4/index.html>Learning Task 4</a></li>
+            <li><a href = LT5/index.html>Learning Task 5</a></li>
+            <li><a href = CLE1MT/index.html>CLE1MT</a></li>
+        </ul>
+    </body>
+                     
+</html>
